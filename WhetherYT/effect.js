dd0b61f0d8d27c42acf3c4832279b269048d9ebb
Pratyush Kumar <pratyushkumardelhi55@gmail.com>
2023-07-14 22:27:59 +0530

    Modified

---
+++ WhetherYT/effect.js
@@ -0,0 +1,21 @@
+const apiKey ="501b0ea58b94af7cb3e836eb524f2c13";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+async function checkWhether(city){
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    var data = await response.json();
+
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+}
+
+searchBtn.addEventListener("click", ()=>{
+    checkWhether(searchBox.value);
+})
